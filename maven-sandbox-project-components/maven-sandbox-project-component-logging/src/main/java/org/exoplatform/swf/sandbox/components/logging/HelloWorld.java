7e0bc0e1d90fbd9539bbf2e0b6e64757dbf1954f
Vincent Sellier <vsellier@exoplatform.com>
2017-09-01 16:39:01 +0200

    try to reproduce javadoc error

--- maven-sandbox-project-components/maven-sandbox-project-component-logging/src/main/java/org/exoplatform/swf/sandbox/components/logging/HelloWorld.java
+++ maven-sandbox-project-components/maven-sandbox-project-component-logging/src/main/java/org/exoplatform/swf/sandbox/components/logging/HelloWorld.java
@@ -4,6 +4,20 @@ package org.exoplatform.swf.sandbox.components.logging;
 public class HelloWorld {
 
 
+    /**
+     * Auto generated method signature for Asynchronous Invocations
+     * <ul>
+     *     <br>
+     *     <li></li>
+     *     <br>
+     * </ul>
+     * </p>
+     * @see com..services.MyClass
+     * @param readField
+
+     * @param requestContext14
+
+     */
     public String getMessage() {
         return message;
     }

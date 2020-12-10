a7d569a92a21fb7d70ac7f0024b7bd7cb00b6b55
GouadriaHanen <hgouadria@exoplatform.com>
2020-12-10 09:13:11 +0100

    PR update

---
+++ maven-sandbox-project-components/maven-sandbox-project-component-logging/src/main/java/PR_Package/First_Pr_Class.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package PR_Package;
+
+/**
+ * @author exo
+ *
+ */
+public class First_Pr_Class {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+  System.out.println("It's my first PR with EXO");
+  }
+
+}
